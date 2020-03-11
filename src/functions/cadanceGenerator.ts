c6485f54d45487850872ee75b99479917cb91d77
gabrielbsanchez <gabriel.sanchez@playsportsnetwork.com>
2020-03-11 12:10:49 +0000

    Add interval of 1 second

--- src/functions/cadanceGenerator.ts
+++ src/functions/cadanceGenerator.ts
@@ -6,17 +6,19 @@ export default async function cadanceGenerator(_req: Request, res: Response): Pr
   //min 0
   //max 120
 
+  setInterval(async() => {
+    var firebaseCadence = await firestoreService.getData('user_metadata', 'cadence');
+
+    var maxCadence = 120;
+    var previousCadence = firebaseCadence.previousCadence;
+    var nextCadence = firebaseCadence.nextCadence;
   
-  var firebaseCadence = await firestoreService.getData('user_metadata', 'cadence');
-
-  var maxCadence = 120;
-  var previousCadence = firebaseCadence.previousCadence;
-  var nextCadence = firebaseCadence.nextCadence;
-
-  firebaseCadence.nextCadence = previousCadence+2;
-  firebaseCadence.previousCadence = nextCadence; 
-
-  await firestoreService.addData('user_metadata', 'cadence', firebaseCadence);
+    firebaseCadence.nextCadence = previousCadence+2;
+    firebaseCadence.previousCadence = nextCadence; 
+  
+    await firestoreService.addData('user_metadata', 'cadence', firebaseCadence);
+  },1000)
+  
   
   res.send(200);
 }
