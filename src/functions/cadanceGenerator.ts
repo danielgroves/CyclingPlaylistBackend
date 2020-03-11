74243349d9784d1d0c8ba16c742f024b52d97015
gabrielbsanchez <gabriel.sanchez@playsportsnetwork.com>
2020-03-11 11:28:38 +0000

    POC to generate cadence number

--- src/functions/cadanceGenerator.ts
+++ src/functions/cadanceGenerator.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from 'express';
 
 export default function cadanceGenerator(_req: Request, res: Response): void {
-  res.send(200);
+
+  //min 0
+  //max 120
+
+  var maxCadence = 120;
+  var previousCadence = 2;
+  var nextCadence = 0;
+  
+  let list: number[] = new Array(maxCadence); 
+  
+  for (var i = previousCadence; i < list.length; i++){
+    nextCadence = i+2;
+    break;
+  }
+
+  const healthStats:JSON = <JSON><unknown>{
+    "previousCadence": previousCadence,
+    "nextCadence" : nextCadence
+}
+  
+  res.send(healthStats);
 }
