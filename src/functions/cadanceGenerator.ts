432e4faf14621bb3cb1f118d876693c5385770ca
gabrielbsanchez <gabriel.sanchez@playsportsnetwork.com>
2020-03-11 15:09:38 +0000

    simplify cadence generation code

--- src/functions/cadanceGenerator.ts
+++ src/functions/cadanceGenerator.ts
@@ -3,13 +3,22 @@ import firestoreService from '../lib/firestore/firestoreService';
 
 export default async function cadanceGenerator(_req: Request, res: Response): Promise<void> {
 
-  var min : number = 70;
-  var max : number = 190;
-  var denominator : number = 4
+  
 
   setInterval(async() => {
     var firebaseCadence = await firestoreService.getData('user_metadata', 'cadence');
 
+    var min : number = firebaseCadence.min;
+    var max : number = firebaseCadence.max;
+    var denominator : number = firebaseCadence.range;
+    var nextCadence = firebaseCadence.nextCadence;
+    
+    while (nextCadence >= min && nextCadence <= max) {
+      firebaseCadence.nextCadence = nextCadence + denominator;
+      break;
+    } 
+
+    /*
     var previousCadence = firebaseCadence.previousCadence + denominator;
     var nextCadence = firebaseCadence.nextCadence;
   
@@ -20,6 +29,7 @@ export default async function cadanceGenerator(_req: Request, res: Response): Pr
 
     if (previousCadence >= max)
       firebaseCadence.previousCadence = min;
+      */
   
     await firestoreService.addData('user_metadata', 'cadence', firebaseCadence);
   },5000)
