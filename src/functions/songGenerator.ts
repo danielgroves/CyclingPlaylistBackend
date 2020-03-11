761546ed703cb807fe5914867ee5cd59d179f103
Daniel Groves <dan.groves@playsportsnetwork.com>
2020-03-11 13:05:02 +0000

    Log everything

--- src/functions/songGenerator.ts
+++ src/functions/songGenerator.ts
@@ -1,15 +1,16 @@
 import firestoreService from '../lib/firestore/firestoreService';
 import { Request, Response } from 'express';
+import logger from '../helpers/logging.helper';
 
 export default async function songGenerator(_req: Request, res: Response): Promise<void> {
   const cadence = await firestoreService.getData('user_metadata', 'cadence');
   const tracks = await firestoreService.queryData('tracks', ['tempo', '>=', cadence.nextCadence]);
 
-  await firestoreService.addData('user_playlist', 'nextSong', {
-    id: tracks[0].id
-  })
+  logger.debug('Got tracks: ' , tracks);
 
-  console.log(tracks);
+  await firestoreService.addData('user_playlist', 'nextSong', {
+    id: tracks[0]
+  })
 
   res.send(200);
 }
