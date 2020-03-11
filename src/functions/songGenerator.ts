07346fc2df5cbf9fa612d56973ad6a14fdc896d2
Daniel Groves <dan.groves@playsportsnetwork.com>
2020-03-11 13:21:09 +0000

    Generate song on cadence change

--- src/functions/songGenerator.ts
+++ src/functions/songGenerator.ts
@@ -1,8 +1,7 @@
 import firestoreService from '../lib/firestore/firestoreService';
-import { Request, Response } from 'express';
 import logger from '../helpers/logging.helper';
 
-export default async function songGenerator(_req: Request, res: Response): Promise<void> {
+export default async function songGenerator(_snap: any, _context: any): Promise<void> {
   const cadence = await firestoreService.getData('user_metadata', 'cadence');
   const tracks = await firestoreService.queryData('tracks', ['tempo', '>=', cadence.nextCadence]);
 
@@ -10,7 +9,5 @@ export default async function songGenerator(_req: Request, res: Response): Promi
 
   await firestoreService.addData('user_playlist', 'playlist', {
     id: tracks[0].id
-  })
-
-  res.send(200);
+  });
 }
